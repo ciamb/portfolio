dcee4c170b46e5ae3e6913400de74095860fbea3
ciamb <andreaciambella@outlook.it>
2025-10-26 19:59:05 +0100

    add security properties

--- src/main/java/it/me/domain/PageContentHomeService.java
+++ src/main/java/it/me/domain/PageContentHomeService.java
@@ -25,15 +25,15 @@ public class PageContentHomeService {
     public PageContent createHomeIfMissing() {
         return pageContentReadBySlugRepository.readBySlug("home")
                 .orElseGet(() -> {
-                    PageContent home = new PageContent();
-                    home.setSlug("home");
-                    home.setTitle("Ciao, sono Andrea, ma per gli amici Ciamb!");
-                    home.setBody("""
-                            Benvenuto nel mio mini-portfolio, costruito con Quarkus - Qute - SQLite!
-                            I testi che vedi arrivano direttamente dal database, e li aggiorno via api quando devo aggiornare le mie competenze!
-                            Buona permanenza :)
-                            """);
-                    home.setUpdatedAt(LocalDateTime.now());
+                    PageContent home = new PageContent()
+                            .setSlug("home")
+                            .setTitle("Ciao, sono Andrea, ma per gli amici Ciamb!")
+                            .setBody("""
+                                    Benvenuto nel mio mini-portfolio, costruito con Quarkus - Qute - SQLite!
+                                    I testi che vedi arrivano direttamente dal database, e li aggiorno via api quando devo aggiornare le mie competenze!
+                                    Buona permanenza :)
+                                    """)
+                            .setUpdatedAt(LocalDateTime.now());
                     em.persist(home);
                     return home;
                 });
